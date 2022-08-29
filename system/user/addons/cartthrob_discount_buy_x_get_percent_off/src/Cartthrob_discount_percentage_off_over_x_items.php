80803011c5ad7475fc77a6b8cb305d69e2e794b9
Eric Lamb <eric@mithra62.com>
2022-08-29 12:40:55 -0700

    updates to use fancy objects and not deprecated/removed methods

--- system/user/addons/cartthrob_discount_buy_x_get_percent_off/src/Cartthrob_discount_percentage_off_over_x_items.php
+++ system/user/addons/cartthrob_discount_buy_x_get_percent_off/src/Cartthrob_discount_percentage_off_over_x_items.php
@@ -1,6 +1,7 @@
 <?php if ( ! defined('CARTTHROB_PATH')) Cartthrob_core::core_error('No direct script access allowed');
 
 use CartThrob\Plugins\Discount\DiscountPlugin;
+use CartThrob\Math\Number;
 
 class Cartthrob_discount_percentage_off_over_x_items extends DiscountPlugin
 {
@@ -22,9 +23,9 @@ class Cartthrob_discount_percentage_off_over_x_items extends DiscountPlugin
 	
 	public function get_discount()
 	{
-		$count = count($this->core->cart->items()); 
-		if ($count >= $this->core->sanitize_number($this->plugin_settings('order_over'))) {
-			return $this->core->cart->subtotal() * ($this->core->sanitize_number($this->plugin_settings('percentage_off')) / 100);
+		$count = count($this->core->cart->items());
+		if ($count >= Number::sanitize($this->plugin_settings('order_over'))) {
+			return $this->core->cart->subtotal() * (Number::sanitize($this->plugin_settings('percentage_off')) / 100);
 		}
 		
 		return 0;
